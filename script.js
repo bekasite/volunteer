9d4ff0ea9a0c0426631bae4afda327c2a25b78fa
Beka Temesgen <beka00499@gmail.com>
2024-06-15 20:38:01 +0300

    updated

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 if(innerWidth<=700){
     document.getElementById("h1").style.marginTop="0px";
     document.getElementById("ul").style.marginTop="36px";
-    document.getElementById("email").style.marginLeft="20px";
-    document.getElementById("signup").style.marginLeft="50px";
-      document.getElementById("name").style.marginLeft="20px";
+   
     }
-  
+  function signup(){
+    window.location.href="signup.html";
+  }

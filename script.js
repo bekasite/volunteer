b94b811e3edf7df6aac8cc9ebc3a11215be88274
Beka Temesgen <beka00499@gmail.com>
2024-06-16 10:23:42 +0300

    nav bar

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 if(innerWidth<=700){
     document.getElementById("h1").style.marginTop="0px";
     document.getElementById("ul").style.marginTop="36px";
-   
+    document.getElementById("img").style.height="130px";
+    document.getElementById("img1").style.height="130px"
+    function bar(){
+
+      document.getElementById("ul").style.width="100%";
+    }
     }
   function signup(){
     window.location.href="signup.html";
+  }
+  function bar(){
+
+    document.getElementById("ul").style.width="27%";
+  }
+  function closed(){
+    document.getElementById("ul").style.width="0px";
   }
